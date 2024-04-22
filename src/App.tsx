1aef65eae91a54a96f46a25b15258a9e143329d6
kemikal <janne@kemikal.org>
2024-04-22 08:38:44 +0200

    env vars

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import './App.css'
 
 function App() {
 
+  console.log("TEST", import.meta.env.VITE_TEST_KEY)
+
   return (
     <>
       <h1>Hello Digital Ocean</h1>
+      This is a {process.env.NODE_ENV} version!
     </>
   )
 }
